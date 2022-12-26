e5b6a981c12f9be3c985a74ec58544eec9a9e1d3
Rucha <ruchalakhmapure@gmail.com>
2022-12-26 15:00:24 +0530

    second commit : new code

---
+++ src/com/demo/LinkedListDemo.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<>();
+		list.add("123");
+		list.add("245");
+		System.out.println(list);
+	}
+
+}
